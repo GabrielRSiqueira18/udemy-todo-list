16b537c316036c8f8e885950a6ebb7c48d542520
GabrielRSiqueira18 <gabrielrsiqueira2023@gmail.com>
2023-11-20 13:55:11 -0300

    feat: create a logic to alter and delete task when using input

--- src/app/modules/home/components/tasks-list/tasks-list.component.ts
+++ src/app/modules/home/components/tasks-list/tasks-list.component.ts
@@ -10,7 +10,7 @@ export class TasksListComponent implements DoCheck {
   public itemsTaskList: TaskList[] = [];
 
   ngDoCheck(): void {
-    this.itemsTaskList.sort((a, b) => Number(a.checked) - Number(b.checked))
+    this.itemsTaskList.sort((a, b) => Number(a.checked) - Number(b.checked));
   }
 
   public setItemInTaskList(taskValue: string) {
@@ -21,11 +21,26 @@ export class TasksListComponent implements DoCheck {
   }
 
   public removeAllTasks() {
-    this.itemsTaskList = []
+    this.itemsTaskList = [];
   }
 
   public deleteTask(index: number) {
-    this.itemsTaskList.splice(index, 1)
+    this.itemsTaskList.splice(index, 1);
+  }
+
+  public validValue(value: string, index: number) {
+    if(!value) {
+      const confirmDeleteItem = window.confirm("Deseja realmente deletar a task?");
+
+      if(confirmDeleteItem) {
+        this.itemsTaskList.splice(index, 1)
+      } else {
+        do {
+          const newValueTask = window.prompt("Digite o novo valor da task")!
+          this.itemsTaskList[index].value = newValueTask
+        } while(!this.itemsTaskList[index].value)
+      }
+    }
   }
 
 }
